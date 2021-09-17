bc4fd497379a39e474b39ab68d28311c8780386d
Sumit Jain <sumeetsuniljain@gmail.com>
2021-09-17 14:09:18 +0530

    added initial page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './sumit-jain-bg.jpg';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Full website coming soon
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="mailto:sumeetsuniljain@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Get in touch
         </a>
       </header>
     </div>
